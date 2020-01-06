2bf82a9e055a7266a792105a17b412e0fe417d00
ngochiendb11 <nguyenngochien11111999@gmail.com>
2020-01-06 12:13:36 +0700

    Thay doi thu muc

---
+++ OBJ/Users.java
@@ -0,0 +1,74 @@
+package OBJ;
+
+public class Users {
+	private int id;
+	private String username;
+	private String password;
+	private String email;
+	private String fullname;
+	private String role;
+	
+	public Users() {
+		
+	}
+
+	public Users(int id, String username, String password, String email, String fullname, String role) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.fullname = fullname;
+		this.role = role;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+}
